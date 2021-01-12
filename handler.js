c84e059269dd3767be611375949ca1fadff15210
Marc Lehmann <mln@dasburo.com>
2021-01-12 09:28:31 +0100

    use js layer

--- handler.js
+++ handler.js
@@ -1,18 +1,15 @@
 'use strict';
+const { helloJs } = require('hello_js_layer')
 
-module.exports.hello = async (event) => {
+module.exports.hello = async(event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+    body: JSON.stringify({
+        message: helloJs(),
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
